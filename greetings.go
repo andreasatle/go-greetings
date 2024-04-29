53283a01499cd15f6443b401c9e655a04e39bca6
Andreas Atle <atle.andreas@gmail.com>
2024-04-29 14:18:27 -0500

    Changed error message

--- greetings.go
+++ greetings.go
@@ -8,7 +8,7 @@ import (
 // Hello returns a greeting for the named person.
 func Hello(name string) (string, error) {
     if name == "" {
-        return "", errors.New("empty name")
+        return "", errors.New("Name is empty")
     }
     // Return a greeting that embeds the name in a message.
     message := fmt.Sprintf("Hi, %v. Welcome!", name)
