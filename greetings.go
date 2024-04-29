7a0e02c3bb72ec1cca1636cbd711e2f248dc9a09
Andreas Atle <atle.andreas@gmail.com>
2024-04-29 15:31:42 -0500

    Randomize messages

--- greetings.go
+++ greetings.go
@@ -3,6 +3,7 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 // Hello returns a greeting for the named person.
@@ -11,6 +12,18 @@ func Hello(name string) (string, error) {
         return "", errors.New("Name is empty")
     }
     // Return a greeting that embeds the name in a message.
-    message := fmt.Sprintf("Hi, %v. Welcome!", name)
+    message := fmt.Sprintf(randomFormat(), name)
     return message, nil
+}
+
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+	// Return a randomly selected message format by specifying
+	// a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
 }
